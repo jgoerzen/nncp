5ca70093d9ae1a6a8bfb312551d569adeb836412
John Goerzen <jgoerzen@complete.org>
2019-10-20 19:07:04 -0500

    removed

--- debian/salsa-ci.yml.ex
+++
@@ -1,6 +0,0 @@
-# For more information on what jobs are run see:
-# https://salsa.debian.org/salsa-ci-team/pipeline
----
-include:
-  - https://salsa.debian.org/salsa-ci-team/pipeline/raw/master/salsa-ci.yml
-  - https://salsa.debian.org/salsa-ci-team/pipeline/raw/master/pipeline-jobs.yml
